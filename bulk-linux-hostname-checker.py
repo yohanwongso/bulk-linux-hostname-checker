f2a9e853dc0276eb5867db4bd6fe557980547a78
Yohan Wongso <40362171+yohanwongso@users.noreply.github.com>
2023-07-23 02:00:09 +0700

    Update bulk-linux-hostname-checker.py

--- bulk-linux-hostname-checker.py
+++ bulk-linux-hostname-checker.py
@@ -1,42 +1,50 @@
+from yaml import safe_load
+from csv import writer
 from paramiko import SSHClient,AutoAddPolicy
-import csv
 
-uname = ""
-pword = ""
+def create_SSH_client (ssh_hostname, ssh_port, ssh_username, ssh_password):
+    try:
+        ssh_client = SSHClient()
+        ssh_client.load_system_host_keys()
+        ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+        ssh_client.connect(hostname=ssh_hostname, port=ssh_port, username=ssh_username, password=ssh_password)
+        return ssh_client
+    except:
+        return False
+    
+def get_host_name (ssh_client):
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command('hostname')
+    hostname = ssh_stdout.read().decode("utf8").replace("\n", "")
 
-list_ip_file = open("ip.txt", "r")
+    ssh_stdin.close()
+    ssh_stdout.close()
+    ssh_stderr.close()
 
-with open('report.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    csv_header = ["IP", "Hostname"]
-    writer.writerow(csv_header)
+    return hostname
 
-    for ip_with_enter in list_ip_file:
-        ip = ip_with_enter.replace("\n", "")
+with open('config.yml', 'r') as yaml_config_file:
+    config = safe_load(yaml_config_file)
+    yaml_config_file.close()
 
-        print (ip)
+    with open(config["ssh"]["list_ip_file"], "r") as list_ip_file:
+        with open(config["output"]["csv"], 'w', newline='', encoding='utf-8') as output_csv_file:
+            output_csv_writer = writer(output_csv_file)
+            csv_header = ["IP", "Hostname"]
+            output_csv_writer.writerow(csv_header)
 
-        try:
-            client = SSHClient()
-            #client.load_system_host_keys()
-            #client.load_host_keys('~/.ssh/known_hosts')
-            client.set_missing_host_key_policy(AutoAddPolicy())
+            for ip_with_enter in list_ip_file:
+                ip = ip_with_enter.replace("\n", "")
+                print(ip)
+                ssh_client = create_SSH_client(ip, config["ssh"]["port"], config["ssh"]["username"], config["ssh"]["password"])
 
-            client.connect(ip, username = uname, password = pword)
+                if ssh_client != False:
+                    hostname = get_host_name(ssh_client)
+                    csv_data = [ip, hostname]
+                    output_csv_writer.writerow(csv_data)
+                else:
+                    csv_data = [ip, ""]
+                    writer.writerow(csv_data)
 
-            stdin, stdout, stderr = client.exec_command('hostname')
-
-            output = stdout.read().decode("utf8").replace("\n", "")
-            csv_data = [ip, output]
-
-            writer.writerow(csv_data)
-
-            stdin.close()
-            stdout.close()
-            stderr.close()
-            client.close()
-        except:
-            csv_data = [ip, ""]
-            writer.writerow(csv_data)
-
-    f.close()
+                ssh_client.close()
+            output_csv_file.close()
+        list_ip_file.close()
